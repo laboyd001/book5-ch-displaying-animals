6cb7aaec7a9bc517b763b3717a200ec10f678ceb
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-16 16:00:26 -0600

    exercise complete and deleted comments

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -3,14 +3,6 @@ import React, {Component} from 'react'
 export default class EmployeeList extends Component {
   render() {
     return (
-      // <article>
-      //           <h1>Employee List</h1>
-      //           <section>Jessica Younker</section>
-      //           <section>Jordan Nelson</section>
-      //           <section>Zoe LeBlanc</section>
-      //           <section>Blaise Roberts</section>
-      // </article>
-
       <section className="employees">
       <h2>Employee List</h2>
         {this.props.employees.map(employee =>
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -3,17 +3,6 @@ import React, {Component} from 'react'
 export default class LocationList extends Component {
   render() {
     return (
-      // <article>
-      //           <h1>Location List</h1>
-                
-      //             <h4>Nashville North</h4>
-      //             <p>123 main st</p>
-                
-                
-      //             <h4>Nashville South</h4>
-      //             <p>456 church st</p>
-                
-      // </article>
       <section className="locations">
       <h2>Location List</h2>
         {this.props.locations.map(location =>
